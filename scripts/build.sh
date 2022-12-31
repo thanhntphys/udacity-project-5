2e303f7b9b1634951693dec7a0b246e0d52c7ae1
thanhntphys <thanhntphys@gmail.com>
2022-12-31 18:45:29 +0700

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,5 @@ set -e
 
 BUILD_NUMBER=$1
 
-docker.exe build -t 296665253765.dkr.ecr.us-east-1.amazonaws.com/udacity-prj5:$BUILD_NUMBER .
-docker.exe push 296665253765.dkr.ecr.us-east-1.amazonaws.com/udacity-prj5:$BUILD_NUMBER
-
+/usr/bin/com.docker.cli build -t 296665253765.dkr.ecr.us-east-1.amazonaws.com/udacity-prj5:$BUILD_NUMBER .
+/usr/bin/com.docker.cli push 296665253765.dkr.ecr.us-east-1.amazonaws.com/udacity-prj5:$BUILD_NUMBER
--- scripts/login-ecr.sh
+++ scripts/login-ecr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-/usr/local/bin/aws ecr get-login-password --region us-east-1 | docker.exe login --username AWS --password-stdin 296665253765.dkr.ecr.us-east-1.amazonaws.com
+/usr/local/bin/aws ecr get-login-password --region us-east-1 | /usr/bin/com.docker.cli login --username AWS --password-stdin 296665253765.dkr.ecr.us-east-1.amazonaws.com
